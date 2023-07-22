257f1fcc70c6cf34d3ad8e423d9d3e584fd21cb2
Diego <diegoj9999@gmail.com>
2023-07-22 15:54:12 -0300

    Fix Fetch error

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -2,5 +2,5 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export const WEATHER_DEFAULT = "Buenos%20Aires";
-export const apiWeather = (city) => `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=3d903356f57d9397bbb908197298d953`;
+export const apiWeather = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 export const apiForecast = (city) =>  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
